c6c7df48f5dbe0c2f2effd049f8402eea807e306
edgar <garinich.7@gmail.com>
2023-11-16 12:58:22 +0600

    remove separator in react select

--- src/common/components/Select.tsx
+++ src/common/components/Select.tsx
@@ -1,9 +1,16 @@
 import { ForwardedRef, forwardRef, ComponentProps } from "react"
-import ReactSelect from "react-select"
+import ReactSelect, { IndicatorSeparatorProps } from "react-select"
+
+const IndicatorSeparator = ({}: IndicatorSeparatorProps) => {
+  return null;
+};
+
+const components = { IndicatorSeparator }
 
 export const Select = forwardRef((props: ComponentProps<typeof ReactSelect>, ref: ForwardedRef<ReactSelect>) => (
   <ReactSelect
     {...props}
+    components={components}
     ref={ref as any}
   />
 ))
